a7b6366f5c22da453a800eba13faa4b24bdd4f63
Luma Verin <verinluma@gmail.com>
2025-08-28 13:31:24 +0000

    messing around with glitch

--- profile-glitch.js
+++ profile-glitch.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const profilePic = document.querySelector('.profile-pic');
-  profilePic.addEventListener('mouseleave', () => {
-    profilePic.classList.remove('glitch-img');
-    void profilePic.offsetWidth;
-    profilePic.classList.add('glitch-img');
-    setTimeout(() => {
-      profilePic.classList.remove('glitch-img');
-    }, 200); // match animation duration
-  });
-  profilePic.addEventListener('mouseenter', () => {
-    profilePic.classList.remove('glitch-img');
-  });
-});
+// profile-glitch.js
+// document.addEventListener('DOMContentLoaded', () => {
+//   const profilePic = document.querySelector('.glitch-img');
+//   profilePic.addEventListener('mouseleave', () => {
+//     profilePic.classList.remove('glitching');
+//     void profilePic.offsetWidth;
+//     profilePic.classList.add('glitching');
+//     setTimeout(() => {
+//       profilePic.classList.remove('glitching');
+//     }, 400); // match animation duration
+//   });
+//   profilePic.addEventListener('mouseenter', () => {
+//     profilePic.classList.remove('glitching');
+//   });
+// });
